965c1b19c9e441a4cf0d33a25f6781190f972848
cam71101 <david.fisher137@gmail.com>
2020-10-17 20:53:42 +0100

    Updates

---
+++ precache-manifest.8fb90f17f2f48b185f810783f503addb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7c389fb571acd3854a2abfb29be3fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73cc488229bdad3f03cf",
+    "url": "/static/css/main.d40e8435.chunk.css"
+  },
+  {
+    "revision": "62dc35e42b9d28f15768",
+    "url": "/static/js/2.6add3f2a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6add3f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cc488229bdad3f03cf",
+    "url": "/static/js/main.366b1e40.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "fd74f389bc5b8e9d5f078792e409ac9f",
+    "url": "/static/media/BattleShips_ScreeGrab_Desktop_Cropped.a0f45325.png"
+  },
+  {
+    "revision": "71dff86ec187c77bd002034353eb0126",
+    "url": "/static/media/My_Movie_List.a78c1de9.png"
+  },
+  {
+    "revision": "e5333c1229a7127b63e6c8350d298018",
+    "url": "/static/media/Weather_App.ed8505b4.png"
+  },
+  {
+    "revision": "d10f4bf68a27526ef83fd9dfdc75317d",
+    "url": "/static/media/cssLogo.d10f4bf6.svg"
+  },
+  {
+    "revision": "a0657d1cac3b20f1e113738056db3c63",
+    "url": "/static/media/htmlLogo.a0657d1c.svg"
+  },
+  {
+    "revision": "dea7a03ff9cb4942a3b1a29625f6d826",
+    "url": "/static/media/javaScriptLogo.dea7a03f.svg"
+  },
+  {
+    "revision": "861aadad40cdf2f50b56641552ccdfa8",
+    "url": "/static/media/materialUILogo.861aadad.svg"
+  },
+  {
+    "revision": "a899d525422ddb1c9b5bc595ec6da942",
+    "url": "/static/media/reactLogo.a899d525.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/static/media/sassLogo.b446ca5a.svg"
+  }
+]);
